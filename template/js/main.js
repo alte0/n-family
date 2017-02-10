7384960cd234fa4db58fe0af4a863a735f66613b
Максим <ctpagateji@yandex.ru>
2017-02-10 19:18:52 +0500

    календарь, логика меню

--- template/js/main.js
+++ template/js/main.js
@@ -2,5 +2,8 @@
 //= ../../bower_components/slick-carousel/slick/slick.js
 //= ../../bower_components/letteringjs/jquery.lettering.js
 //= ../../bower_components/textillate/jquery.textillate.js
+//= ../../bower_components/jquery-modal/jquery.modal.js
+//= ../../bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js
+//= ../../bower_components/bootstrap-datepicker/dist/locales/bootstrap-datepicker.ru.min.js
 
 //= parts/code.js
--- template/js/parts/code.js
+++ template/js/parts/code.js
@@ -16,76 +16,116 @@
 			return false;
 		});
 
-
-		var desktop = '1000';
+		//перехватываем нажатие на контакты на десктопе, что бы открыть выпадающие инфо контактов
+		var desktop = '1915', itemLinkContact = $('#contact'), panelContact = $('#contacts');
 		function widthSreen(widthWindow) {
 			if (widthWindow >= desktop) {
-				// console.log(widthWindow);
+				console.log(widthWindow);
 				console.log(desktop);
+				itemLinkContact.on('click', function(event) {
+					// event.preventDefault();
+					panelContact.css('display', 'block');
+					$(window).scrollTop(0);
+					return false;
+					/* Act on the event */
+				});
 			}
 		}
-
-		$(window).resize(function() {
-	    var widthWindow = $(window).width();
+		// узнаем ширину для перехвата
+		var widthWindow = $(window).width();
+		$(window).ready(widthSreen(widthWindow)).resize(function() {
+	    var widthWindow = $(this).width();
 	    widthSreen(widthWindow);
 		});
 
-		// var $reviews = $('#reviews');
-		// var $animateText = $reviews.find('.animate-text').textillate({
-		//   // the default selector to use when detecting multiple texts to animate
-		//   selector: '.texts',
-		//   // enable looping
-		//   loop: true,
-		//   // sets the minimum display time for each text before it is replaced
-		//   minDisplayTime: 1000,
-		//   // sets the initial delay before starting the animation
-		//   // (note that depending on the in effect you may need to manually apply
-		//   // visibility: hidden to the element before running this plugin)
-		//   initialDelay: 0,
-		//   // set whether or not to automatically start animating
-		//   autoStart: false,
-		//   // in animation settings
-		//   in: {
-		//     // set the effect name
-		//     effect: 'fadeIn',
-		//     // set the delay factor applied to each consecutive character
-		//     delayScale: 1.5,
-		//     // set the delay between each character
-		//     delay: 100,
-		//     // set to true to animate all the characters at the same time
-		//     sync: false,
-		//     // randomize the character sequence
-		//     // (note that shuffle doesn't make sense with sync = true)
-		//     shuffle: false,
-		//     // reverse the character sequence
-		//     // (note that reverse doesn't make sense with sync = true)
-		//     reverse: false
-		//   },
-		//   // out animation settings.
-		//   out: {
-		//     effect: 'fadeOut',
-		//     delayScale: 0.1,
-		//     delay: 50,
-		//     sync: false,
-		//     shuffle: false,
-		//     reverse: true,
-		//     callback: function () {}
-		//   },
+		//закрываем выпадаюшие инфо
+		closeButtonPanelContact = $('#contacts-close');
+		closeButtonPanelContact.click(function(event) {
+			/* Act on the event */
+			panelContact.css('display', 'none');
+			return false;
+		});
 
-		//   // callback that executes once textillate has finished
-		//   callback: function () {},
+		// анимация текста
+		var $reviews = $('#reviews');
+		var $animateText = $reviews.find('.animate-text').textillate({
+		  // the default selector to use when detecting multiple texts to animate
+		  selector: '.texts',
+		  // enable looping
+		  loop: true,
+		  // sets the minimum display time for each text before it is replaced
+		  minDisplayTime: 500,
+		  // sets the initial delay before starting the animation
+		  // (note that depending on the in effect you may need to manually apply
+		  // visibility: hidden to the element before running this plugin)
+		  initialDelay: 1000,
+		  // set whether or not to automatically start animating
+		  autoStart: false,
+		  // in animation settings
+		  in: {
+		    // set the effect name
+		    effect: 'fadeIn',
+		    // set the delay factor applied to each consecutive character
+		    // delayScale: 1.5,
+		    // set the delay between each character
+		    delay: 100,
+		    // set to true to animate all the characters at the same time
+		    sync: false,
+		    // randomize the character sequence
+		    // (note that shuffle doesn't make sense with sync = true)
+		    shuffle: false,
+		    // reverse the character sequence
+		    // (note that reverse doesn't make sense with sync = true)
+		    reverse: false
+		  },
+		  // out animation settings.
+		  out: {
+		    effect: 'fadeOut',
+		    delay: 0,
+		    sync: false,
+		    shuffle: false,
+		    reverse: true,
+		    callback: function () {}
+		  },
 
-		// });;
+		  // callback that executes once textillate has finished
+		  callback: function () {},
 
+		});;
+
+		  $animateText.textillate('start');
 		// $reviews.find('.reviews-family, .reviews-all').hover(function() {
 		// 	/* Stuff to do when the mouse enters the element */
 		//   $animateText.textillate('start');
 		// }, function() {
 		// 	/* Stuff to do when the mouse leaves the element */
-		// 	$animateText.textillate('stop');
+		// 	$animateText.textillate('out');
 		// });
-		
 
+		//натройка модалки
+		 // $("#order-form").modal({
+			// 	showClose: false
+			// });
+
+		//
+    $('#datepicker').datepicker({
+    language: "ru",
+    todayHighlight: true,
+    beforeShowDay: function (date){
+          if (date.getMonth() == (new Date()).getMonth())
+            switch (date.getDate()){
+              case 4:
+                return {
+                  tooltip: 'Example tooltip',
+                  classes: 'active'
+                };
+              case 8:
+                return false;
+              case 12:
+                return "green";
+          }
+        }
+    });
 			//slick
 			$('.header-slider').slick({
 				dots: true,
